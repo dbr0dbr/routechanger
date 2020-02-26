60cfc4bb46a2003ea56d3ad50d19b5a687a94cd3
andrey.chubukov <dbr0dbr@gmail.com>
2020-02-26 13:42:06 +0200

    second upload2

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 from provider import Provider
+
 first = Provider(name ='WNet', 
                 gatway = '192.168.30.10', 
                 test_hosts = ['8.26.56.8', '37.235.1.174'])
+
 second = Provider(name = 'DTS',
                  gatway =  "192.168.30.254", 
                  test_hosts = ['8.20.247.20', '37.235.1.177'])
-log = "/var/log/routechanger.log"
+
+log = "/var/log/routechanger.log1"
